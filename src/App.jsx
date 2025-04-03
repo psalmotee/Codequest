0be00d48d5418b7dbe6ab1bd61e946c2096b1994
psalmotee <samsonmoradeyo@gmail.com>
2025-04-03 12:55:14 +0100

    CodeQuest Progress

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/select-account" element={<SelectAccount />} />
+      {/* <Route path="/select-account" element={<SelectAccount />} />
       <Route path="/signup" element={<SignUp />} />
-      <Route path="/login" element={<SignIn />} />
+      <Route path="/login" element={<SignIn />} /> */}
     </Routes>
   );
 }
